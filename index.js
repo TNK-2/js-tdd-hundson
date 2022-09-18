c14198f82676a3c9c0a60ba28954b4e85c1577b8
TNK-2 <yuuu1993g@gmail.com>
2022-09-18 19:20:39 +0900

    add test

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+function fizzBuzz(num) {
+  return num.toString();
+}
+
+module.exports = {"add": add, "fizzBuzz": fizzBuzz};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,14 @@
-const add = require('./index');
+const obj = require('./index');
 
 test('１と２を渡したとき、３を返す', () => {
-  expect(add(1,2)).toBe(3);
-})
+  expect(obj.add(1,2)).toBe(3);
+});
+
+test("1渡すと文字列'1'を返す", () => {
+  const actual = obj.fizzBuzz(1);
+  expect(actual).toBe('1');
+});
+
+test("2を渡すと文字列’2’を返す", () => {
+  expect(obj.fizzBuzz(2)).toBe('2');
+});
